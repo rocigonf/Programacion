be356ac9b0d3ef1433c68a73784069bd3b99ce43
Rocío Gonfaus Luengo <116120484+RocioGonfausLuengo@users.noreply.github.com>
2022-11-03 16:39:54 +0100

    Add files via upload

---
+++ Ej9.java
@@ -0,0 +1,23 @@
+/*
+ * Ej9.java
+ * 
+ * Copyright 2022 Rocio Alejandra Gonfaus Luengo
+ * 
+ */
+
+
+public class Ej9 {
+	
+	public static void main (String[] args) {
+		
+		
+		System.out.println(" ■■■   ■■■ ");
+		System.out.println("■■■■■ ■■■■■");
+		System.out.println(" ■■■■■■■■■");
+		System.out.println("  ■■■■■■■");
+		System.out.println("   ■■■■■");
+		System.out.println("    ■■■");
+		System.out.println("     ■ ");
+	}
+}
+
