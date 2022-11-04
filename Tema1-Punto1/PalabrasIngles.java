7385b26cee4940088243d3e12aedc1ee9d38c071
Rocío Gonfaus Luengo <116120484+RocioGonfausLuengo@users.noreply.github.com>
2022-11-04 15:48:36 +0100

    Add files via upload

---
+++ Tema1-Punto1/PalabrasIngles.java
@@ -0,0 +1,23 @@
+/*
+ * PalabrasIngles.java
+ * 
+ * @author Rocio Alejandra Gonfaus Luengo
+ * 
+ */
+ 
+ public class PalabrasIngles {
+	 
+	 public static void main (String[] args){
+		 
+		 System.out.println("cat         gato");
+		 System.out.println("house       casa");
+		 System.out.println("screen      pantalla");
+		 System.out.println("mouse       raton");
+		 System.out.println("apple       manzana");
+		 System.out.println("laptop      portatil");
+		 System.out.println("witch       bruja");
+		 System.out.println("pumpkin     calabaza");
+		 System.out.println("ghost       fantasma");
+		 System.out.println("hand        mano");
+	 }
+ }
