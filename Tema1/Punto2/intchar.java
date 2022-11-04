aba0f76c8c5c910f35249828d44a244afd62a3ec
Rocío Gonfaus Luengo <116120484+RocioGonfausLuengo@users.noreply.github.com>
2022-11-04 16:03:03 +0100

    Create intchar.java

---
+++ Tema1/Punto2/intchar.java
@@ -0,0 +1,22 @@
+/*
+ * intchar.java
+ * 
+ * Correspondencia entre los tipos char e int.
+ * 
+ * @author 2022 Rocio Alejandra Gonfaus Luengo
+ * 
+ */
+ 
+public class intchar {
+	
+	public static void main(String[] args) {
+		
+		char letra1 = 'a';
+		char letra2 = 'b';
+		
+		System.out.println(letra1);
+		System.out.println(letra2);
+		System.out.println(letra1 + letra2);
+		System.out.println(letra1 + "" + letra2);
+	}
+}
