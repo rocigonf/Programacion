8a14bb8ad06ac12f35ee70457d80420f2e938204
Rocío Gonfaus Luengo <116120484+RocioGonfausLuengo@users.noreply.github.com>
2022-11-04 15:50:12 +0100

    Create Nombre.java

---
+++ Tema1/Punto1/Nombre.java
@@ -0,0 +1,15 @@
+/*
+ * Nombre.java
+ * 
+ * @author Rocio Alejandra Gonfaus Luengo
+ * 
+ */
+
+
+public class Nombre {
+	
+	public static void main (String[] args) {
+		
+		System.out.print("Mi nombre es Rocio");
+	}
+}
