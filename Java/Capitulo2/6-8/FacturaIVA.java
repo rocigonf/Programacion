85c7d98c794e2c449a79ea3b1c938afed9a8b043
Rocío Gonfaus Luengo <116120484+rocigonf@users.noreply.github.com>
2022-11-07 17:49:03 +0100

    Update FacturaIVA.java

--- Java/Capitulo2/6-8/FacturaIVA.java
+++ Java/Capitulo2/6-8/FacturaIVA.java
@@ -17,11 +17,15 @@ public class FacturaIVA {
 		int baseImp = 657;
 		double IVA = 1.21;
 		
+		//Sin casting.
+		double decimales = baseImp * IVA;
+		
+		//Con casting.
 		int total;
 		total = (int) (baseImp * IVA);
 		
 		System.out.println("El precio sin IVA es de " + baseImp + " euros y el precio con IVA es de " + total + " euros");
+		System.out.println("Sin casting el resultado total es de " + decimales + " euros");
 		
 	}
 }
-
